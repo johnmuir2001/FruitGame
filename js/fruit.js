14af8e05c23b96c66efbb2bffd4576ec2559d0ae
John Muir <johnmuir@Johns-MacBook-Pro.local>
2020-02-07 17:30:32 +0000

    working balance

--- js/fruit.js
+++ js/fruit.js
@@ -23,10 +23,10 @@ function spin() {
   //Check if the reels match up by checking 1 and 0 & 0 and 2
   if (final[0] == final[1] && final[1] == final[2]) {
     document.getElementById("status").src = "images/BigWin.png";
-    balance = balance - bet.value;
+    balance = balance += parseInt(bet.value);
   } else {
     document.getElementById("status").src = "images/Fail.png";
-    balance = balance += bet.value;
+    balance = balance - parseInt(bet.value);
   }
   update_theScrene();
   console.log(bet.value)

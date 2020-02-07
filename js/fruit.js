3c0549e95349b10db299df0eaf36c6300ee77a14
John Muir <johnmuir@Johns-MacBook-Pro.local>
2020-02-07 16:54:15 +0000

    first commit

---
+++ js/fruit.js
@@ -0,0 +1,57 @@
+//Mr Seagull Wrote this - IT IS AWESOMMMMMMMEEEEEEEEEEE
+
+document.getElementById("Start").addEventListener("click", spin);
+
+//Set balnce to 500
+let balance = 500;
+let bet = document.getElementById("bet");
+
+const increaseBet = () => {
+  bet = bet + 2;
+};
+
+function spin() {
+  balance = balance - bet;
+  //alert(bet)
+  let final = [];
+  final.push(spinReel("r1"));
+  final.push(spinReel("r2"));
+  final.push(spinReel("r3"));
+  //Check if the reels match up by checking 1 and 0 & 0 and 2
+  if (final[0] == final[1] && final[1] == final[2]) {
+    document.getElementById("status").src = "images/BigWin.png";
+  } else {
+    document.getElementById("status").src = "images/Fail.png";
+  }
+  update_theScrene();
+  console.log(bet.value)
+}
+function spinReel(reel) {
+  let i = Math.floor(Math.random() * 7);
+  if (i == 0) {
+    document.getElementById(reel).src = "images/OneBar.png";
+  }
+  if (i == 1) {
+    document.getElementById(reel).src = "images/Grapes.png"; 
+  }
+  if (i == 2) {
+    document.getElementById(reel).src = "images/Lemon.png"; 
+  }
+  if (i == 3) {
+    document.getElementById(reel).src = "images/Orange.png"; 
+  }
+  if (i == 4) {
+    document.getElementById(reel).src = "images/Strawberry.png"; 
+  }
+  if (i == 5) {
+    document.getElementById(reel).src = "images/Watermelon.png"; 
+  }
+  if (i == 6) {
+    document.getElementById(reel).src = "images/ThreeBars.png"; 
+  }
+  return i;
+}
+
+const update_theScrene = () => {
+  document.getElementById("balanceDisplay").innerHTML = balance;
+};
